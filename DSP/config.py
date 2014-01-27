a732d26ffcdc0cdf04617dd0332de63a5046b9e2
Fan Jin <fan.jin@mail.mcgill.ca>
2014-01-27 12:57:03 -0500

    adjusted config to DAC box device name

--- DSP/config.py
+++ DSP/config.py
@@ -3,7 +3,7 @@ import pyaudio
 class audio(object):
 
     host_api = 'ALSA'
-    host_api_device = 'default'
+    host_api_device = 'Scarlett 2i2 USB: USB Audio (hw:1,0)'
     sampling_rate = 44100
     input_channels = 1
     sampling_format = pyaudio.paFloat32
