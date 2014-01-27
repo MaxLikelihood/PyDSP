3da1b99b90877971bdb51c95c627a1cfef363d25
Fan Jin <fan.jin@mail.mcgill.ca>
2014-01-27 01:05:50 -0500

    privatized findHostAPI() and findDevice()

--- DSP/API.py
+++ DSP/API.py
@@ -1,5 +1,5 @@
+from config import audio as config_audio
 import pyaudio
-import config
 import time
 
 class audio(object):
@@ -10,28 +10,28 @@ class audio(object):
     instantiate = False
 
     def setup(self):
-        if self.instantiate:
+        if audio.instantiate:
             # avoid duplicated invocation
             return
         # instantiate PyAudio
         self.p = pyaudio.PyAudio()
         # locate host_api by name provided in config
-        self.host_api_index = self.findHostAPI()
+        self.host_api_index = self.__findHostAPI()
         if self.host_api_index == -1:
-            print "\nInvalid Host API: %s" % config.host_api
+            print "\nInvalid Host API: %s" % config_audio.host_api
         else:
-            print "\nValid Host API: %s" % config.host_api
+            print "\nValid Host API: %s" % config_audio.host_api
             # locate host_api_device by name provided in config
-            self.host_api_device_index = self.findDevice()
+            self.host_api_device_index = self.__findDevice()
             if self.host_api_device_index == -1:
-                print "\nInvalid Host API Device: %s" % config.host_api_device
+                print "\nInvalid Host API Device: %s" % config_audio.host_api_device
             else:
-                print "\nValid Host API Device: %s" % config.host_api_device
+                print "\nValid Host API Device: %s" % config_audio.host_api_device
                 try:
-                    self.supported = self.p.is_format_supported(rate = config.sampling_rate,
+                    self.supported = self.p.is_format_supported(rate = config_audio.sampling_rate,
                                                                 input_device = self.host_api_device_index,
-                                                                input_channels = config.input_channels,
-                                                                input_format = config.sampling_format,
+                                                                input_channels = config_audio.input_channels,
+                                                                input_format = config_audio.sampling_format,
                                                                 output_device = None,
                                                                 output_channels = None,
                                                                 output_format = None)
@@ -48,7 +48,7 @@ class audio(object):
         # terminate PyAudio
         self.p.terminate()
 
-    def findHostAPI(self):
+    def __findHostAPI(self):
         # locate the host api specified in config
         # return index if found, otherwise return -1
         host_api_count = self.p.get_host_api_count()
@@ -57,12 +57,12 @@ class audio(object):
         else:
             for i in range(host_api_count):
                 host_api_info = self.p.get_host_api_info_by_index(i)
-                if host_api_info['name'] == config.host_api:
+                if host_api_info['name'] == config_audio.host_api:
                     return i
             else:
                 return -1
 
-    def findDevice(self):
+    def __findDevice(self):
         # locate the device within the specified host api
         # return index if found, otherwise return -1
         self.host_api_device_count = self.p.get_host_api_info_by_index(self.host_api_index)['deviceCount']
@@ -71,7 +71,7 @@ class audio(object):
         else:
             for i in range(self.host_api_device_count):
                 device_info = self.p.get_device_info_by_host_api_device_index(self.host_api_index, i)
-                if device_info['name'] == config.host_api_device:
+                if device_info['name'] == config_audio.host_api_device:
                     return i
             else:
                 return -1
