755aff47a38cd35698229de28edb2191bf41afbe
Fan Jin <fan.jin@mail.mcgill.ca>
2014-01-26 22:38:32 -0500

    added input device name field in configuration file

--- DSP/config.py
+++ DSP/config.py
@@ -3,12 +3,14 @@ import pyaudio
 class config(object):
 
     global host_api
+    global host_api_device
     global sampling_rate
     global input_channels
     global sampling_format
 
     def __init__(self):
         self.host_api = 'ALSA'
+        self.host_api_device = 'redbox'
         self.sampling_rate = 44100
         self.input_channels = 1
         self.sampling_format = pyaudio.paFloat32
