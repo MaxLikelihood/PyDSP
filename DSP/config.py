d2d703bd5f2317402e74b512203d6badca4d8093
Fan Jin <fan.jin@mail.mcgill.ca>
2014-01-26 23:23:09 -0500

    changed device to default

--- DSP/config.py
+++ DSP/config.py
@@ -10,7 +10,7 @@ class config(object):
 
     def __init__(self):
         self.host_api = 'ALSA'
-        self.host_api_device = 'redbox'
+        self.host_api_device = 'default'
         self.sampling_rate = 44100
         self.input_channels = 1
         self.sampling_format = pyaudio.paFloat32
