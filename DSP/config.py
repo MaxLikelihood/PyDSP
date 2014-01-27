52bf07c20b7c7f4b6291b8a106dc0eccd2228b3c
Fan Jin <fan.jin@mail.mcgill.ca>
2014-01-27 01:11:56 -0500

    added frame_per_buffer parameter to config.py

--- DSP/config.py
+++ DSP/config.py
@@ -1,16 +1,10 @@
 import pyaudio
 
-class config(object):
+class audio(object):
 
-    global host_api
-    global host_api_device
-    global sampling_rate
-    global input_channels
-    global sampling_format
-
-    def __init__(self):
-        self.host_api = 'ALSA'
-        self.host_api_device = 'default'
-        self.sampling_rate = 44100
-        self.input_channels = 1
-        self.sampling_format = pyaudio.paFloat32
+    host_api = 'ALSA'
+    host_api_device = 'default'
+    sampling_rate = 44100
+    input_channels = 1
+    sampling_format = pyaudio.paFloat32
+    frame_per_buffer = 1024
