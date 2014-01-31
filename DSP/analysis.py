2af4d0ef0209e0c853a93b6c239e54217a9939ff
Fan Jin <fan.jin@mail.mcgill.ca>
2014-01-31 16:00:30 -0500

    Add privatized rfft function

--- DSP/analysis.py
+++ DSP/analysis.py
@@ -1,6 +1,7 @@
 from config import audio as config_audio
 import numpy
 from scipy import signal
+from scipy.fftpack import rfft
 
 class audio(object):
 
@@ -46,8 +47,24 @@ class audio(object):
         for i in range(len(data)):
             data[i]['frame_windowed'] = data[i]['frame_decoded'][:] * window
 
+
     @staticmethod
     def analyze(data):
         # Decode & window raw audio data
         audio.__decode_data(data)
         audio.__window_data(data)
+
+    @staticmethod
+    def __rfft_data(data):
+        # Apply discrete fourier transform on windowed audio data & store as new key:value pair in dictionary
+        # Parameters: data: [{'frame_data': string,
+        #                     'frame_count': int,
+        #                     'frame_time': float,
+        #                     'frame_position': int,
+        #                     'frame_decoded': type
+        #                     'frame_windowed': type}, ...]
+
+        for i in range(len(data)):
+            data[i]['frame_rfft'] = rfft(data[i]['frame_windowed'])
+
+
