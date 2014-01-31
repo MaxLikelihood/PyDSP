ac2c2863e195f1e3755877c8c9cdb6885fd672c2
Fan Jin <fan.jin@mail.mcgill.ca>
2014-01-31 11:01:44 -0500

    Add variable decoding_format for analysis.py __decode_data()

--- DSP/config.py
+++ DSP/config.py
@@ -3,8 +3,10 @@ import pyaudio
 class audio(object):
 
     host_api = 'ALSA'
-    host_api_device = 'Scarlett 2i2 USB: USB Audio (hw:1,0)'
+    # host_api_device = 'Scarlett 2i2 USB: USB Audio (hw:1,0)'
+    host_api_device = 'default'
     sampling_rate = 44100
     input_channels = 1
     sampling_format = pyaudio.paFloat32
+    decoding_format = 'Float32'
     frames_per_buffer = 1024
