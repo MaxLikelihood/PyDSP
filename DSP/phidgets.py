ec8fbf47e6c3091baa6ac3768682ffc04dd8f7e0
Fan Jin <fan.jin@mail.mcgill.ca>
2014-02-09 02:36:12 -0500

    Add import of configurations for stepper motor

--- DSP/phidgets.py
+++ DSP/phidgets.py
@@ -1,6 +1,7 @@
 from Phidgets.PhidgetException import PhidgetErrorCodes, PhidgetException
 from Phidgets.Events.Events import AttachEventArgs, DetachEventArgs, ErrorEventArgs, InputChangeEventArgs, CurrentChangeEventArgs, StepperPositionChangeEventArgs, VelocityChangeEventArgs
 from Phidgets.Devices.Stepper import Stepper as Phidgets_stepper
+from config import stepper as config_stepper
 from time import sleep
 
 class stepper(object):
@@ -14,4 +15,3 @@ class stepper(object):
     # indicate device virtual position
     __virtual_position = 0
 
-    
